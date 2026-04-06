9cb434b418118f8090679bcd437d1454710f14d0
Hyeri Jo <johello62@gmail.com>
2026-04-06 20:12:04 +0900

    [level 2] Title: 피로도, Time: 3.30 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,3 +1,14 @@
+/**
+ * 접근: DFS로 방문 가능한 던전을 하나씩 선택하면서 최대 방문 개수를 구한다.
+ * 시간복잡도: O(n!)
+ *
+ * 풀이 메모:
+ * - 각 던전을 한 번씩만 방문할 수 있으므로 visited 배열로 방문 여부를 관리
+ * - 현재 피로도로 입장 가능한 던전만 선택해서 다음 DFS로 진행
+ * - count는 전역변수가 아니라 파라미터로 전달해서 각 탐색 경로의 상태를 독립적으로 유지
+ * - 탐색이 끝나면 visited를 false로 되돌려 다른 경우의 수를 탐색
+ */
+
 import java.util.*;
 
 class Solution {
@@ -12,7 +23,7 @@ class Solution {
             
             if (!visited[j] && k >= a) {
                 visited[j] = true;
-                dfs(k-b, visited, count + 1, dungeons);
+                dfs(k - b, visited, count + 1, dungeons);
                 visited[j] = false;
             }
         }
@@ -20,13 +31,7 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
-        int count = 0;
-        
-        for(int i = 0; i < dungeons.length; i++) {
-            dfs(k, visited, count, dungeons);
-            result = Math.max(result, count);
-            count = 0;
-        }
+        dfs(k, visited, 0, dungeons);
         
         return result;
     }
